990fc3e473dbb874f31dd28adc7a707541fd015d
Lv-jw-cont <879386004@qq.com>
2022-03-14 19:17:13 +0800

    20220314

--- myprojact/main.py
+++ myprojact/main.py
@@ -1,58 +1,7 @@
-from distutils.command.clean import clean
-import os
 import math
-from time import sleep, time
+import os
+import random
 
-
-
-def filedoc(filename:str,doc:bool = False) -> str:
-    finddoc = filename.rfind('.')
-    if not doc :
-        finddoc += 1
-    return filename[finddoc:] if finddoc > 0 else ''
-# if __name__ == '__main__':
-#      print(filedoc('asc.py'))
-
-def calcu ( init_value:int , fn , *args:int , **kwargs:int ) -> int:
-    total = init_value
-    for arg in args:
-        if type(arg) in (int,float):
-            total = fn(total,arg)
-    for varg in kwargs.values():
-        if type(varg) in (int,float):
-            total = fn(total,varg)
-    return total
-# if __name__ == '__main__':
-#      print(calcu(100,lambda x , y : x + y ,111))
-
-def sort(lst:list,fn=lambda x , y : x > y ,resver:bool=False) -> list:
-    new_lst = lst[:]
-    for i in range (1,len(new_lst)):
-        swap = False
-        for j in range (0,len(new_lst) - i ):
-            if fn (new_lst[j],new_lst[j + 1]):
-                new_lst[j],new_lst[j+1] = new_lst[j+1],new_lst[j]
-                swap = True
-        if not swap :
-            break
-    return new_lst
-# if __name__ == '__main__':
-#      print(sort([11,53,73,24,73,57,64,23,78]))
-
-def midfind(lst:list,nmb:int) -> int:
-    start ,end = 0 ,len(lst) -1
-    while start <= end:
-        mid = (start + end) // 2
-        if nmb > lst[mid]:
-            start = mid + 1
-        elif nmb < lst[mid]:
-            end = mid - 1
-        else:
-            return mid
-    return -1
-# if __name__ == '__main__':
-    # print(midfind([11, 23, 24, 53, 57, 64, 73, 73, 78],78))
-     
 # class Student:
 
 #     def __init__(self,name:str,age:int,sex:str) -> None:
@@ -118,3 +67,51 @@ class Clock:
 #         sleep(1)
 #         clock.run()
 
+class Card:
+    def __init__(self,clor,num) -> None:
+        self.clor = clor
+        self.num = num
+
+    def __repr__(self) -> str:
+        return self.show()
+
+    def show(self):
+        suites =  {'s': '♠','h':'♥','c':'♣','d':'♦'}
+        faces = ['','A','2','3','4','5','6','7','8','9','10','J','Q','K']
+        return f'{suites[self.clor]}{faces[self.num]}'
+
+
+def exit():
+    a = Card('s',2)
+    print(a)
+
+# if __name__ == '__main__':
+#     exit()
+
+class Poke:
+
+    def __init__(self):
+        self.poke_list = []
+        for su in 'shcd':
+            for fa in range(1,14):
+                car = Card(su,fa)
+                self.poke_list.append(car)
+        self.count = 0
+    
+    def shuffle(self) -> list: 
+        self.count = 0
+        random.shuffle(self.poke_list)
+
+    def give_cars(self):
+        cars = self.poke_list[self.count]
+        self.count += 1
+        return cars
+
+    def has_more(self) -> bool: 
+        return self.count < len(self.poke_list)
+
+lst = Poke()
+lst.shuffle()
+print(lst.poke_list)
+while lst.has_more():
+    print (lst.give_cars())
